142bf0b1ec8e48db9c620cda83323497971627de
Sanna-Ali <133492348+Sanna-Ali@users.noreply.github.com>
2025-07-09 11:02:29 +0300

    Update create-product.dto.ts

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,5 +1,3 @@
-import { CanActivate, Injectable } from '@nestjs/common';
-import { CanActivate, Injectable } from '@nestjs/common';
 import {
   IsString,
   IsNotEmpty,
@@ -21,6 +19,3 @@ export class CreateProductDto {
   @Min(0, { message: 'price should not be less zero' })
   price: number;
 }
-
-@Injectable()
-export class AuthGuard implements CanActivate {}
